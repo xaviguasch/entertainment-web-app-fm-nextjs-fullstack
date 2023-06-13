ec8e91a8525dacc2467dc9590e9491eba372ab2a
Xavi Guasch <xaviguasch@gmail.com>
2023-06-13 15:18:31 +0200

    added animations and finished styling on Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,6 +7,12 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
 
+import BookmarkIcon from '../components/icons/BookmarkIcon'
+import HomeIcon from '../components/icons/HomeIcon'
+import MoviesIcon from './icons/MoviesIcon'
+import TvSeriesIcon from './icons/TvSeries'
+import LogoIcon from './icons/LogoIcon'
+
 import classes from './Navbar.module.css'
 
 // unable to change SVG fill via CSS currently
@@ -17,45 +23,27 @@ const Navbar = () => {
   return (
     <div className={classes.Navbar}>
       <Link href='/'>
-        <Image priority src='/images/logo.svg' alt='logo icon' width={25} height={20} />
+        <LogoIcon />
       </Link>
 
       <nav className={classes.nav}>
         <Link href='/'>
-          <Image
-            className={`${currentRoute === '/' ? classes.activeIcon : null}`}
-            priority
-            src='/images/icon-nav-home.svg'
-            alt='home icon'
-            width={16}
-            height={16}
-          />
+          <HomeIcon classNameProp={`${currentRoute === '/' ? 'activeIcon' : null}`} />
         </Link>
         <Link href='/movies'>
-          <Image
-            priority
-            src='/images/icon-nav-movies.svg'
-            alt='movies icon'
-            width={16}
-            height={16}
+          <MoviesIcon
+            classNameProp={`${currentRoute === '/movies' ? 'activeIcon' : null}`}
           />
         </Link>
         <Link href='/tvseries'>
-          <Image
-            priority
-            src='/images/icon-nav-tv-series.svg'
-            alt='movies icon'
-            width={16}
-            height={16}
+          <TvSeriesIcon
+            classNameProp={`${currentRoute === '/tvseries' ? 'activeIcon' : null}`}
           />
         </Link>
+
         <Link href='/bookmarks'>
-          <Image
-            priority
-            src='/images/icon-nav-bookmark.svg'
-            alt='movies icon'
-            width={16}
-            height={16}
+          <BookmarkIcon
+            classNameProp={`${currentRoute === '/bookmarks' ? 'activeIcon' : null}`}
           />
         </Link>
       </nav>
