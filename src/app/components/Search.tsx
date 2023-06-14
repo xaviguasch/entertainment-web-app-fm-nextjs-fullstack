1ca7ac826d398c7db776f934a903354fb5df695f
Xavi Guasch <xaviguasch@gmail.com>
2023-06-14 20:42:45 +0200

    added Search component

---
+++ src/app/components/Search.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './Search.module.css'
+
+const Search = () => {
+  return <div>Search Component</div>
+}
+
+export default Search
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import Search from './components/Search'
 
 export default function Home() {
   return (
     <main>
       <h1>Entertainment Web App</h1>
+      <Search />
     </main>
   )
 }
