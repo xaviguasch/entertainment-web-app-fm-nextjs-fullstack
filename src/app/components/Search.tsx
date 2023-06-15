90a40f10fd5d893613aff79a568bb03ef6bffad0
Xavi Guasch <xaviguasch@gmail.com>
2023-06-15 10:41:12 +0200

    set up env files and API keys

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import classes from './Search.module.css'
 
 const Search = () => {
-  return <div>Search Component</div>
+  const [searchInput, setSearchInput] = useState('')
+
+  function searchInputHandler(e) {
+    setSearchInput(e.target.value)
+  }
+
+  return (
+    <div>
+      <form action=''>
+        <label htmlFor='searchInput'></label>
+        <input
+          type='text'
+          id='searchInput'
+          value={searchInput}
+          onChange={searchInputHandler}
+        />
+      </form>
+    </div>
+  )
 }
 
 export default Search
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Search from './components/Search'
 export default function Home() {
   return (
     <main>
-      <h1>Entertainment Web App</h1>
       <Search />
     </main>
   )
