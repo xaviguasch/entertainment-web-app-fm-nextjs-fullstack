35e64856823aed3879e6853df17069591663be43
Xavi Guasch <xaviguasch@gmail.com>
2023-06-17 20:35:02 +0200

    wired a fetching function to the Context provider

--- src/app/context/EntAppProvider.tsx
+++ src/app/context/EntAppProvider.tsx
@@ -1,13 +1,32 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
+
+import { searchAPI } from '../../../lib/tmdb'
 
 export const EntAppContext = createContext()
 
 function EntAppProvider({ children }) {
   const [cartItems, setCartItems] = useState(['la', 'li'])
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedData = await searchAPI()
+        console.log('hola')
+        console.log(fetchedData)
+        setData(fetchedData)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchData()
+  }, [])
 
   let entAppState = {
     cartItems,
     setCartItems,
+    data,
   }
 
   return <EntAppContext.Provider value={entAppState}>{children}</EntAppContext.Provider>
