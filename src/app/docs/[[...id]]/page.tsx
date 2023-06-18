d6811cd4e626f44338f28cf40bbf89f479c657f4
Xavi Guasch <xaviguasch@gmail.com>
2023-06-18 13:18:54 +0200

    deleted auth routes

---
+++ src/app/docs/[[...id]]/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React, { useState } from 'react'
+
+const DocsPage = ({ params }) => {
+  console.log(params)
+
+  return (
+    <div>
+      <h1>DocsPAge</h1>
+    </div>
+  )
+}
+
+export default DocsPage
