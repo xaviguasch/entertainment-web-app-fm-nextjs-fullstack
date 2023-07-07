ba332970c0879f82ebb7fb928ee781e838a04de3
Xavi Guasch <xaviguasch@gmail.com>
2023-07-07 20:36:57 +0200

    added Bookmarked component

---
+++ src/app/bookmarked/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './page.module.css'
+
+const Bookmarked = () => {
+  return <div className={classes.Bookmarked}>Bookmarked</div>
+}
+
+export default Bookmarked
