a279bcc247c8ba75f1dceb62959e52b3e6ed3116
Xavi Guasch <xaviguasch@gmail.com>
2023-07-24 12:20:37 +0200

    created ItemInfo component

---
+++ src/app/components/ItemInfo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './ItemInfo.module.css'
+
+const ItemInfo = () => {
+  return <div className={classes.itemInfo}>ItemInfo</div>
+}
+
+export default ItemInfo
