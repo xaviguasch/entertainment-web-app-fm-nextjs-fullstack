daaa1724f5a1c9495d6419197a1ac275f1a0ee2f
Xavi Guasch <xaviguasch@gmail.com>
2023-08-24 21:58:41 +0200

    refactored Trending component to solve TS errors

--- src/app/components/Trending.tsx
+++ src/app/components/Trending.tsx
@@ -3,19 +3,25 @@ import classes from './Trending.module.css'
 import TrendingItem from './TrendingItem'
 import { getTrendingCat } from '../utils/fetchData'
 
+import { ContentItemProps } from '../types/Types.types'
+
 const Trending = async () => {
-  const trendingTitles = await getTrendingCat('all')
+  const trendingTitles: ContentItemProps[] | undefined = await getTrendingCat('all')
 
   console.log('------------')
   console.log(trendingTitles)
   console.log('------------')
+
+  if (!trendingTitles) {
+    return null
+  }
 
   return (
     <div className={classes.Trending}>
       <h2 className='section-title'>Trending</h2>
 
       <div className={classes.horizontal}>
-        {trendingTitles.map((item: object) => {
+        {trendingTitles.map((item: ContentItemProps) => {
           return <TrendingItem key={item.id} data={...item} />
         })}
       </div>
