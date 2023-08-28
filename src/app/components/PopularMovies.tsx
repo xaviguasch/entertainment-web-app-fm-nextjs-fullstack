2609fd352e8684379be8a797dbcd119f3a623505
Xavi Guasch <xaviguasch@gmail.com>
2023-08-28 16:12:49 +0200

    solved error in PopularMovies and PopularTVSeries referring to mapping undefined values

--- src/app/components/PopularMovies.tsx
+++ src/app/components/PopularMovies.tsx
@@ -14,21 +14,22 @@ export default async function PopularMovies() {
       <h2 className='section-title'>Popular Movies</h2>
 
       <div className={classes.popularCols}>
-        {movies
-          .map((item: ContentItemProps) => {
-            return (
-              <Item
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                image={item.backdrop_path}
-                year={+item.release_date.split('-')[0]}
-                rating={+item.vote_average}
-                category='movie'
-              />
-            )
-          })
-          .slice(0, 12)}
+        {movies.length > 0 &&
+          movies
+            .map((item: ContentItemProps) => {
+              return (
+                <Item
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  image={item.backdrop_path}
+                  year={+item.release_date.split('-')[0]}
+                  rating={+item.vote_average}
+                  category='movie'
+                />
+              )
+            })
+            .slice(0, 12)}
       </div>
     </div>
   )
--- src/app/components/PopularTvSeries.tsx
+++ src/app/components/PopularTvSeries.tsx
@@ -14,21 +14,22 @@ export default async function PopularTvSeries() {
       <h2 className='section-title'>Popular TV Series</h2>
 
       <div className={classes.popularCols}>
-        {series
-          .map((item: ContentItemProps) => {
-            return (
-              <Item
-                key={item.id}
-                id={item.id}
-                title={item.name}
-                image={item.backdrop_path}
-                year={+item.first_air_date.split('-')[0]}
-                rating={+item.vote_average}
-                category='tv'
-              />
-            )
-          })
-          .slice(0, 12)}
+        {series.length > 0 &&
+          series
+            .map((item: ContentItemProps) => {
+              return (
+                <Item
+                  key={item.id}
+                  id={item.id}
+                  title={item.name}
+                  image={item.backdrop_path}
+                  year={+item.first_air_date.split('-')[0]}
+                  rating={+item.vote_average}
+                  category='tv'
+                />
+              )
+            })
+            .slice(0, 12)}
       </div>
     </div>
   )
