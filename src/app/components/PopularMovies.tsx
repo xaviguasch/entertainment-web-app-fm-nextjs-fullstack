4189da5f4ccde814dc13cb353aa870e47215f2b1
Xavi Guasch <xaviguasch@gmail.com>
2023-08-28 16:23:45 +0200

    solved error in PopularMovies and PopularTVSeries referring to mapping undefined values, part 3

--- src/app/components/PopularMovies.tsx
+++ src/app/components/PopularMovies.tsx
@@ -14,7 +14,7 @@ export default async function PopularMovies() {
       <h2 className='section-title'>Popular Movies</h2>
 
       <div className={classes.popularCols}>
-        {movies.length > 0 &&
+        {movies &&
           movies
             .map((item: ContentItemProps) => {
               return (
