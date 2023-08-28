e50ad69649bd986ee6cc0a7b93bb8cacaee45b95
Xavi Guasch <xaviguasch@gmail.com>
2023-08-28 15:56:43 +0200

    another TS deployment testing

--- src/app/components/Trending.tsx
+++ src/app/components/Trending.tsx
@@ -6,21 +6,21 @@ import { getTrendingCat } from '../utils/fetchData'
 import { ContentItemProps } from '../types/Types.types'
 
 const Trending = async () => {
-  const trendingTitles: ContentItemProps[] | undefined = await getTrendingCat('all')
+  // const trendingTitles: ContentItemProps[]  = await getTrendingCat('all')
 
-  console.log('------------')
-  console.log(trendingTitles)
-  console.log('------------')
+  // console.log('------------')
+  // console.log(trendingTitles)
+  // console.log('------------')
 
-  if (!trendingTitles) {
-    return null
-  }
+  // if (!trendingTitles) {
+  //   return null
+  // }
 
   return (
     <div className={classes.Trending}>
       <h2 className='section-title'>Trending</h2>
 
-      <div className={classes.horizontal}>
+      {/* <div className={classes.horizontal}>
         {trendingTitles.map((item: ContentItemProps) => {
           return (
             <TrendingItem
@@ -34,7 +34,7 @@ const Trending = async () => {
             />
           )
         })}
-      </div>
+      </div> */}
     </div>
   )
 }
